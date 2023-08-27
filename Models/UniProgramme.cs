7df0b6d9a2f413e9be8053c75159d5b0d8983c87
Saaid Faraz <farazsaaid@gmail.com>
2023-08-27 21:52:07 +1200

    UniProgramme model created.

--- Models/UniProgramme.cs
+++ Models/UniProgramme.cs
@@ -1,6 +1,16 @@
-﻿namespace UniPlanner.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPlanner.Models
 {
     public class UniProgramme
     {
+        [Key]
+        public required int UniProgrammesID { get; set; }
+        public required UniversityInfo UniversityID { get; set; }
+        public required Programme ProgrameID { get; set; }
+        public required string Link { get; set; }
+        [Range(0, 320, ErrorMessage = "Please enter correct value")]
+        public int? RankScore { get; set; }
+        public required ICollection<MajorsOffered> MajorsOffereds { get; set; }
     }
 }
