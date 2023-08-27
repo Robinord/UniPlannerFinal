e7684f4c4062ded26c3a4902ea93d7aef6591b16
Saaid Faraz <farazsaaid@gmail.com>
2023-08-27 21:49:45 +1200

    UniversityInfo model created.

--- Models/UniversityInfo.cs
+++ Models/UniversityInfo.cs
@@ -1,6 +1,18 @@
-﻿namespace UniPlanner.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniPlanner.Models
 {
     public class UniversityInfo
     {
+        [Key]
+        public int UniversityInfoID { get; set; }
+        public required string Name { get; set; }
+        public required string City { get; set; }
+        public required string Region { get; set; }
+        public required int THErank { get; set; }
+        public required int QSrank { get; set; }
+        public required int ARWUrank { get; set; }
+
+        public ICollection<UniProgramme> UniProgrammes { get; set; }
     }
 }
