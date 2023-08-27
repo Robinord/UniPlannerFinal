5de5b8c59264c4e8ae58a259404253801465b9d4
Saaid Faraz <farazsaaid@gmail.com>
2023-08-27 21:50:48 +1200

    Programme model defined.

--- Models/Programme.cs
+++ Models/Programme.cs
@@ -1,6 +1,15 @@
-﻿namespace UniPlanner.Models
+﻿using System.ComponentModel.DataAnnotations;
+using UniPlanner.Models;
+
+
+namespace UniPlanner.Models
 {
     public class Programme
     {
+        [Key]
+        public required int ProgrammesID { get; set; }
+        public required string Name { get; set; }
+
+        public ICollection<UniProgramme> UniProgrammes { get; set; }
     }
 }
