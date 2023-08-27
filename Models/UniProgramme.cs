7aa46e14c922a85c10dc9a3e2c5478797dc0d689
Saaid Faraz <farazsaaid@gmail.com>
2023-08-27 21:55:07 +1200

    Fixed spelling mistake in UniProgramme.

--- Models/UniProgramme.cs
+++ Models/UniProgramme.cs
@@ -7,7 +7,7 @@ namespace UniPlanner.Models
         [Key]
         public required int UniProgrammeID { get; set; }
         public required UniversityInfo UniversityID { get; set; }
-        public required Programme ProgrameID { get; set; }
+        public required Programme ProgrammeID { get; set; }
         public required string Link { get; set; }
         [Range(0, 320, ErrorMessage = "Please enter correct value")]
         public int? RankScore { get; set; }
